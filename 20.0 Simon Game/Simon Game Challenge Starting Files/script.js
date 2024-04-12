401d386bc096cf93539715c435211081014d9816
kunalarya873 <kunalarya873@gmail.com>
2024-04-12 21:00:34 +0530

    Adding Simon game for jQuery

---
+++ 20.0 Simon Game/Simon Game Challenge Starting Files/script.js
@@ -0,0 +1,17 @@
+var randomNumber =  Math.random();
+randomNumber = randomNumber *3;
+randomNumber = Math.floor(randomNumber);
+const buttonColors = ["red", "blue", "green", "yellow"];
+buttonColors.push()
+
+
+$('#green').on( 'click', function() {
+    document.getElementById('gmusic').play(0);
+});
+$('#red').on( 'click', function() {
+    document.getElementById('rmusic').play(0);
+});$('#blue').on( 'click', function() {
+    document.getElementById('bmusic').play(0);
+});$('#yellow').on( 'click', function() {
+    document.getElementById('ymusic').play(0);
+});

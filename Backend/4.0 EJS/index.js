053269b4aab6278a3fce3df2d66afd084e155a73
kunalarya873 <kunalarya873@gmail.com>
2024-04-18 20:34:38 +0530

    Adding EJS(i)

---
+++ Backend/4.0 EJS/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = 3000;
+app.get("/", (req, res)=>{
+    res.render("./views/index.ejs", {
+        dayType: "a weekday",
+advice: "it's time to work hard"
+});
+});
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
